698c6a24f0a395078f12cccd4bb9549dcf737f80
Rodolfo Escalante <rodolfokenlly@gmail.com>
2024-09-22 22:46:30 -0500

    ✅ Update to express application

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
-// src/app.js
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+// const port = 3000;
 
-exports.handler = async (event) => {
-  // Simple handler for demonstration
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Lambda!' }),
-  };
-};
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("ACCEDIENDO AL MAIN PRINCIPAL.");
+});
+
+app.get('/product-details', (req, res) => {
+  res.send("Estas accediendo a la lista de detalles de productos");
+});
+
+app.get('/product-last', (req, res) => {
+  res.send("Estas accecediendo a los ultimos productos mas valorados");
+});
+
+app.get('/product-rate', (req, res) => {
+  res.send("Estas accediendo al promedio de productos mas sorteados");
+});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+module.exports.handler = serverless(app);
